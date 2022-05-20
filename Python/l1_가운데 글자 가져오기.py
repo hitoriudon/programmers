7724a1840b83c3489bdff4552eb205409fda546c
hitoriudon <tjrcjf9@hanyang.ac.kr>
2022-05-20 14:21:27 +0900

    5/20 update

--- Python/l1_가운데 글자 가져오기.py
+++ Python/l1_가운데 글자 가져오기.py
@@ -1,3 +1,3 @@
-def solution(s):
+def solution(s): #my
     n = len(s)
     return s[n//2] if n%2==1 else s[n//2-1:n//2+1]
---
+++ Python/l1_문자열 내림차순으로 배치하기.py
@@ -0,0 +1,4 @@
+def solution(s): #my
+    return ''.join(sorted(s, reverse=True))
+
+''' 리스트는 sort, 문자열은 sorted'''
